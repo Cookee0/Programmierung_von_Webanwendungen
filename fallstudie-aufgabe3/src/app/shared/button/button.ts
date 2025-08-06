2a1dab1cc65683377d2b3a57c196601d2cd88d32
Julian Wagner <julian.wagner2@iubh-dualesstudium.de>
2025-08-07 00:53:08 +0200

    Initial commit

---
+++ fallstudie-aufgabe3/src/app/shared/button/button.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-button',
+  imports: [RouterOutlet],
+  templateUrl: './button.html',
+  styleUrl: './button.css'
+})
+export class Button {
+
+}
